48fc407acd3eca531ddd042b88f44405bd701a18
djalexkidd <alexpyr@outlook.fr>
2021-12-16 11:56:29 +0100

    zooooooooooooom

--- scripts/Level1.gd
+++ scripts/Level1.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var bullet_ressource = preload("res://scenes/Bullet.tscn")
+var dezoom
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("fire") and Global.kitchen_gun: #Quand le clic gauche est appuyé
@@ -8,4 +9,11 @@ func _physics_process(delta):
 		add_child(bullet) #L'ajoute dans la scène
 
 func _on_ChangeCamera_body_entered(body):
-	$Bee/Camera2D.limit_top = -1000000
+	if !dezoom:
+		dezoom = true
+		$Bee/Camera2D.limit_top = -1000000
+		$AnimationPlayer.play("zoom")
+	else:
+		dezoom = false
+		$Bee/Camera2D.limit_top = -420
+		$AnimationPlayer.play("dezoom")
