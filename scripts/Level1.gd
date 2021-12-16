e759fc5b9a4a4ae7ba6ef344472cf8a824f2874d
djalexkidd <alexpyr@outlook.fr>
2021-12-16 11:28:58 +0100

    terrier

--- scripts/Level1.gd
+++ scripts/Level1.gd
@@ -6,3 +6,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("fire") and Global.kitchen_gun: #Quand le clic gauche est appuyé
 		var bullet = bullet_ressource.instance() #Instance en tant que noeud
 		add_child(bullet) #L'ajoute dans la scène
+
+func _on_ChangeCamera_body_entered(body):
+	$Bee/Camera2D.limit_top = -1000000
