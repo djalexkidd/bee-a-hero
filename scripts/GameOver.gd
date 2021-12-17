056396816d7aebe47d8c3d21456ad76c1cae3b8a
djalexkidd <alexpyr@outlook.fr>
2021-12-17 13:23:11 +0100

    Musique niveau

--- scripts/GameOver.gd
+++ scripts/GameOver.gd
@@ -4,4 +4,4 @@ func _on_RetryButton_pressed():
 	get_tree().change_scene("res://scenes/Level1.tscn")
 
 func _on_QuitButton_pressed():
-	get_tree().quit()
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
